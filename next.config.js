a9e0ded4a85574e63e5ee726d1729db3fe738143
mahmoud <mzughbor@gmail.com>
2025-11-28 22:28:23 +0200

    Fix path aliases in Next.js config: Add explicit webpack alias configuration for ~ paths

--- next.config.js
+++ next.config.js
@@ -10,9 +10,15 @@ const nextConfig = {
         fs: false,
       };
     }
+    
+    // Add support for TypeScript path aliases
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '~': require('path').resolve(__dirname, 'src'),
+    };
+    
     return config;
   },
 };
 
 module.exports = nextConfig;
-
